bc0b2447c10c308505c120a654aaf73b31815091
Arya Padmawar <aryapadmawar45@gmail.com>
2026-08-05 22:47:48 +0530

    Merged the frontend branch with main

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a3490f2916e98209"; }
+/******/ 		__webpack_require__.h = function() { return "5a9d890782584e21"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
